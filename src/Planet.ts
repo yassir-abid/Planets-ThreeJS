bb8aafa4a8f4695c2c522a8822d7d9832e326fff
yassir-abid <abid.yassir@gmail.com>
2022-09-06 13:34:54 +0200

    draw spheres representing planets using ThreeJS

--- src/Planet.ts
+++ src/Planet.ts
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 interface Param {
     color: string,
     size: number,
@@ -7,6 +9,11 @@ class Planet {
 
     color: string;
     size: number;
+    scene: THREE.Scene;
+    camera: THREE.PerspectiveCamera;
+    sphere: THREE.Mesh;
+    renderer: THREE.WebGLRenderer;
+    light: THREE.PointLight;
 
     constructor(param: Param) {
         this.color = param.color;
@@ -14,7 +21,29 @@ class Planet {
     }
 
     draw() {
-        console.log(`I want to draw a planet with these properties: color-${this.color}, size-${this.size}`);
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(45, this.calculScreenRatio(), 1, 1000);
+        this.camera.position.z = 15;
+        this.scene.add(this.camera);
+
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(this.renderer.domElement);
+
+        const geometry = new THREE.SphereGeometry(this.size, 128, 64);
+        const material = new THREE.MeshLambertMaterial({ color: this.color });
+        this.sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(this.sphere);
+
+        this.light = new THREE.PointLight();
+        this.light.position.set(-50, 50, 50);
+        this.scene.add(this.light);
+
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    calculScreenRatio(): number {
+        return window.innerWidth / window.innerHeight;
     }
 }
 
